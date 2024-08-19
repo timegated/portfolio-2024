b60f58be77231ea1043c4c2f0ef053c274008b65
timegated <beccdaniel@gmail.com>
2024-08-19 13:25:11 -0700

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://dbeccaria.info',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
